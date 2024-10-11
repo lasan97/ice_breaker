19977fecedba658732eefbc7b4a0124b194456ac
Martin <martin@niceday.io>
2024-10-11 18:27:34 +0900

    create output parser

--- ice_breaker.py
+++ ice_breaker.py
@@ -5,6 +5,7 @@ from langchain.chains import LLMChain
 import os
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import summary_parser
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.twitter import scrape_user_tweets
@@ -16,23 +17,26 @@ def ice_break_with(name: str) -> str:
     twitter_username = twitter_lookup_agent(name=name)
     tweets = scrape_user_tweets(username=twitter_username, mock=True)
 
-    summery_template = """
+    summary_template = """
     given the information about a person from linkedin {information},
     and their latest twitter posts {twitter_posts} I want you to create:
     1. A short summary
     2. two interesting facts about them 
 
     Use both information from twitter and Linkedin
+    \n{format_instructions}
     """
 
-    summery_prompt_template = PromptTemplate(
+    summary_prompt_template = PromptTemplate(
         input_variables=["information", "twitter_posts"],
-        template=summery_template
+        template=summary_template,
+        partial_variables={"format_instructions":summary_parser.get_format_instructions()}
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    chain = LLMChain(llm=llm, prompt=summery_prompt_template)
+    # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    chain = summary_prompt_template | llm | summary_parser
 
     res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
 
